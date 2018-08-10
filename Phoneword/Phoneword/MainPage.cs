59e7d83ae264bca9ded86f075062e247538c8f43
cj-prog <cj-prog@users.noreply.github.com>
2018-08-10 20:56:15 +0200

    daily commit. Next lets have a look of how to share the MainPage

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Phoneword
+{
+    public partial class MainPage
+    {
+        private Forms forms;
+        public string TranslatedNumber;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            forms = new Forms(this);
+        }
+
+        public void OnTranslate(object sender, EventArgs e)
+        {
+            TranslatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(TranslatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + TranslatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        public async void OnCall(object sender, EventArgs e)
+        {
+            if (await forms.FormsDisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + TranslatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                var dialer = forms.GetDependencyService();
+                if (dialer != null)
+                    dialer.Dial(TranslatedNumber);
+            }
+        }
+    }
+}
---
+++ Phoneword/Phoneword/MainPageObjects.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Phoneword
+{
+    public class MainPageObjects // load objects
+    {
+        public class CallButton
+        {
+            public bool IsEnabled;
+            public string Text;
+        }
+
+        public class PhoneNumberText
+        {
+            public string Text;
+        }
+
+        public void InitializeComponent()
+        {
+        }
+
+        public CallButton callButton = new CallButton();
+        public PhoneNumberText phoneNumberText = new PhoneNumberText();
+        public object sender = new object();
+        public EventArgs e = new EventArgs();
+    }
+}
