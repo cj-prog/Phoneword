288f901f05bbc753e770b38d60ad1742cd4c366b
cj-prog <cj-prog@users.noreply.github.com>
2018-08-11 13:03:59 +0200

    ADD: Working example of Phoneword.UWP without use of XAMARIN.forms. Now lets copy this project into cshtml5.

---
+++ Phoneword/Phoneword.UWP/Forms.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Xamarin.Forms;
+
+namespace Phoneword.UWP
+{
+    public class Forms
+    {
+        private MainPage Page;
+
+        public Forms(MainPage page)
+        {
+            Page = page;
+        }
+
+        public async void FormsDisplayAlert(string title, string message, string accept, string cancel)
+        {
+            var messageDialog = new MessageDialog(message);
+            messageDialog.Commands.Add(new UICommand(
+                accept,
+                new UICommandInvokedHandler(this.CommandInvokedHandler)));
+            messageDialog.Commands.Add(new UICommand(
+                "Close",
+                new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+
+            // Set the command that will be invoked by default
+            messageDialog.DefaultCommandIndex = 0;
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 1;
+
+            // Show the message dialog
+            await messageDialog.ShowAsync();
+
+        }
+
+        private void CommandInvokedHandler(IUICommand command)
+        {
+            // Display message showing the label of the command that was invoked
+            Page.NotifyUser("The '" + command.Label + "' command has been selected.");
+        }
+
+        public IDialer GetDependencyService()
+        {
+            return DependencyService.Get<IDialer>();
+        }
+        
+
+    }
+}
---
+++ Phoneword/Phoneword.UWP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+
+namespace Phoneword.UWP
+
+{
+    public partial class MainPage
+    {
+        private Forms forms;
+        public string TranslatedNumber;
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            forms = new Forms(this);
+        }
+
+        public void OnTranslate(object sender, RoutedEventArgs e)
+        {
+            TranslatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(TranslatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Content = "Call " + TranslatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Content = "Call";
+            }
+        }
+        public void NotifyUser(string strMessage)
+        {
+        }
+        
+        public void OnCall(object sender, RoutedEventArgs e)
+        {
+            forms.FormsDisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + TranslatedNumber + "?",
+                "Yes",
+                "No");
+        }
+    }
+}
