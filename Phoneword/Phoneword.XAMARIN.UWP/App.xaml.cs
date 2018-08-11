4d0435465b53793f5152f7116f62da87c3c1b54b
cj-prog <cj-prog@users.noreply.github.com>
2018-08-11 10:33:46 +0200

    Fixed rename failure. False App was loaded in Phoneword.XAMARIN.UWP: App() instead of Phoneword.App()

--- Phoneword/Phoneword.XAMARIN.UWP/App.xaml.cs
+++ Phoneword/Phoneword.XAMARIN.UWP/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Phoneword.UWP
+namespace Phoneword.XAMARIN.UWP
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- Phoneword/Phoneword.XAMARIN.UWP/MainPage.xaml.cs
+++ Phoneword/Phoneword.XAMARIN.UWP/MainPage.xaml.cs
@@ -13,7 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Phoneword.UWP
+namespace Phoneword.XAMARIN.UWP
 {
     public sealed partial class MainPage
     {
