8ab21efdc3962d6e234da2c7d4123d3dccfb84e4
cj-prog <cj-prog@users.noreply.github.com>
2018-09-24 23:42:04 +0200

    Blazor kein Bild

--- Phoneword/Phoneword.Client/Forms.cs
+++ Phoneword/Phoneword.Client/Forms.cs
@@ -1,11 +1,16 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Blazor.Browser.Interop;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Components;
 using Phoneword;
+using Phoneword.Client.Interfaces;
 
 namespace Phoneword.Client
 {
     public class Forms
     {
+        [Inject]
+        private IMessageBoxProvider MessageBoxProvider { get; set; }
+
         private MainPage Page;
 
         public Forms(MainPage page)
@@ -13,10 +18,21 @@ namespace Phoneword.Client
             Page = page;
         }
 
+        //public Task<bool> FormsDisplayAlert(string title, string message, string accept, string cancel)
+        //{
+        //    RegisteredFunction.Invoke<bool>("messageBox", title, message, accept, cancel);
+        //    return null;
+        //}
+
         public Task<bool> FormsDisplayAlert(string title, string message, string accept, string cancel)
         {
-            RegisteredFunction.Invoke<bool>("messageBox", title, message, accept, cancel);
-            return null;
+
+            if (string.IsNullOrEmpty(cancel))
+                throw new ArgumentNullException(nameof(cancel));
+            return MessageBoxProvider.ShowMessageBox(title, message);
+
+
+
         }
 
         public IDialer GetDependencyService()
---
+++ Phoneword/Phoneword.Client/Interfaces/IMessageBoxProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Phoneword.Client.Interfaces
+{
+    public interface IMessageBoxProvider
+    {
+        Task<bool> ShowMessageBox(string title, string content);
+    }
+}
